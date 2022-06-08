5433abdf64682203afa00233580320714956420b
Lou <tongxi.lou@gmail.com>
2022-06-08 22:23:40 +0200

    refactor and comment

--- vacation.py
+++ vacation.py
@@ -8,18 +8,30 @@ import yaml
 import pandas
 
 
-def grab_holiday(url: str) -> list:
+def grab_holiday(year: int, code: str) -> list:
+    '''
+    Get the list of public holidays from internet.
+
+    Args:
+        year: the year to look for.
+        code: the region/conutry to check.
+    Return:
+        List of strings representing the date of public holidays.
+    '''
+    url = f'https://www.qppstudio.net/publicholidays{year}/{code}.htm'
     pattern = '<time datetime="\d\d\d\d-\d\d-\d\d">'
     text = urllib.request.urlopen(url).read().decode('utf8')
     matches = re.findall(pattern, text)
     return set(it[21:-2] for it in matches)
 
 
-def build_url(year: int, code: str) -> str:
-    return f'https://www.qppstudio.net/publicholidays{year}/{code}.htm'
-
-
 def load_config() -> dict:
+    '''
+    Get the configuration, like the year, countries/regions.
+
+    Return:
+        Configuration.
+    '''
     expected = {'countries': list, 'persons': list, 'year': int, 'output': str}
     config = yaml.safe_load(open('config.yml'))
     assert all(it in config and type(config[it]) == expected[it]
@@ -36,7 +48,7 @@ def main():
     }
 
     holidays = {
-        country: grab_holiday(build_url(this_year, country))
+        country: grab_holiday(this_year, country)
         for country in countries.keys()
     }
 
@@ -65,29 +77,38 @@ def main():
     # https://stackoverflow.com/questions/36694313/pandas-xlsxwriter-format-header
     pandas.io.formats.excel.ExcelFormatter.header_style = None
 
+    taken = '  '
+    weekend = ' '
+    free = ''
+    colorization = {
+        taken: 'background-color: lightblue',
+        weekend: 'background-color: lightgrey',
+    }
+
     with pandas.ExcelWriter(config['output'], engine='xlsxwriter') as writer:
         for month, month_data in data.items():
             dates = sorted(list(month_data.keys()))
             to_write = {'Date': dates}
             to_write.update({
                 country_display: [
-                    ' ' if country_code in month_data[date] else ''
+                    taken if country_code in month_data[date] else free
                     for date in dates
                 ]
                 for country_code, country_display in countries.items()
             })
             to_write['Weekend'] = [
-                ' ' if 'weekend' in month_data[date] else '' for date in dates
+                weekend if 'weekend' in month_data[date] else free
+                for date in dates
             ]
             to_write.update({
-                person: [''] * len(month_data)
+                person: [free] * len(month_data)
                 for person in config['persons']
             })
-            pandas.DataFrame(to_write).style.applymap(
-                lambda x: 'background-color: grey'
-                if x == ' ' else '').to_excel(writer,
-                                              sheet_name=month,
-                                              index=False)
+            pandas.DataFrame(to_write).style.applymap(lambda x: colorization[
+                x] if x in colorization else '').to_excel(writer,
+                                                          sheet_name=month,
+                                                          index=False)
 
 
-main()
+if __name__ == '__main__':
+    main()
